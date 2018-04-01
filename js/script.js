3810999b0ed54b5affd842116a7101b3b617bf88
Jerry <jermur11@gmail.com>
2018-04-01 23:26:16 +0100

    Add about section

--- js/script.js
+++ js/script.js
@@ -6,5 +6,12 @@ $(document).ready(function () {
         pagination: false,
     });
 
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer", "Available For Hire"],
+        typeSpeed: 80,
+        loop: true,
+        startDelay: 1500,
+        showCursor: false
+    });
 
 });
